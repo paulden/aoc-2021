c221580f2bf013ed2ce84be431bf01b5cf521adb
Paul de Nonancourt <paul.de.nonancourt@gmail.com>
2021-12-24 18:09:15 +0100

    Day 24 (big big big wip but I got the stars!)

--- README.md
+++ README.md
@@ -8,3 +8,5 @@ Source: https://adventofcode.com/2021/
 - Check private / public identifiers (see https://yourbasic.org/golang/public-private/)
 - Implement Github Action to build, test and get badges
 - Refactor day 23 because it is the worst thing I have ever done and it takes forever to run
+- Actually day 24 is worse so also refactor this one
+
---
+++ src/day24.go
@@ -0,0 +1,137 @@
+package main
+
+import (
+	"strconv"
+	"strings"
+)
+
+func GenerateAllPossibleNumbers() (sequences [][]int) {
+	for i := 11111111111111; i <= 99999999999999; i++ {
+		sequence := make([]int, 0)
+		for k := 1; k <= 14; k++ {
+			sequence = append(sequence, (i/k)%10)
+		}
+		isValid := true
+		for _, j := range sequence {
+			if j == 0 {
+				isValid = false
+			}
+		}
+		if isValid {
+			sequences = append(sequences, sequence)
+		}
+	}
+	return
+}
+
+func RunALUProgram(instructions []ALUInstruction, sequence []int, w, x, y, z int) (int, int, int, int) {
+	//z = 2
+	pointer := 0
+
+	for _, instruction := range instructions {
+		var a *int
+		var b *int
+
+		switch instruction.a {
+		case "w":
+			a = &w
+		case "x":
+			a = &x
+		case "y":
+			a = &y
+		case "z":
+			a = &z
+		}
+		switch instruction.b {
+		case "w":
+			b = &w
+		case "x":
+			b = &x
+		case "y":
+			b = &y
+		case "z":
+			b = &z
+		}
+
+		if instruction.isInp() {
+			//fmt.Printf("\nAssigning input %v: %v\n", sequence[pointer], instruction)
+			*a = sequence[pointer]
+			pointer++
+			continue
+		}
+
+		var secondTerm int
+		i, err := strconv.ParseInt(instruction.b, 10, 64)
+		if err == nil {
+			secondTerm = int(i)
+		} else {
+			secondTerm = *b
+		}
+
+		if instruction.isAdd() {
+			*a += secondTerm
+		}
+		if instruction.isMul() {
+			*a *= secondTerm
+		}
+		if instruction.isDiv() {
+			*a = *a / secondTerm
+		}
+		if instruction.isMod() {
+			*a = *a % secondTerm
+		}
+		if instruction.isEql() {
+			if *a == secondTerm {
+				*a = 1
+			} else {
+				*a = 0
+			}
+		}
+		//fmt.Printf("%v - %v, %v, %v, %v\n", instruction, w, x, y, z)
+	}
+
+	return w, x, y, z
+}
+
+func ParseALUInstructions(input []string) (instructions []ALUInstruction) {
+	for _, instruction := range input {
+		split := strings.Split(instruction, " ")
+		var newInstruction ALUInstruction
+		if len(split) >= 3 {
+			newInstruction = ALUInstruction{split[0], split[1], split[2]}
+		} else {
+			newInstruction = ALUInstruction{split[0], split[1], ""}
+		}
+		instructions = append(instructions, newInstruction)
+	}
+	return
+}
+
+type ALUInstruction struct {
+	instruction string
+	a, b        string
+}
+
+func (i *ALUInstruction) isInp() bool {
+	return i.instruction == "inp"
+}
+
+func (i *ALUInstruction) isAdd() bool {
+	return i.instruction == "add"
+}
+
+func (i *ALUInstruction) isMul() bool {
+	return i.instruction == "mul"
+}
+
+func (i *ALUInstruction) isDiv() bool {
+	return i.instruction == "div"
+}
+
+func (i *ALUInstruction) isMod() bool {
+	return i.instruction == "mod"
+}
+
+func (i *ALUInstruction) isEql() bool {
+	return i.instruction == "eql"
+}
